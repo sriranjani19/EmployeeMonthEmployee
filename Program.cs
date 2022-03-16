d85cf7e3a5d312ab814a56e9da39a7558a9e09b8
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-16 15:42:02 +0530

    program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Using OOPS");
+
+            //EmployeeAttendance empAttendance = new EmployeeAttendance();
+            //empAttendance.PresentorAbsent();
+
+            //EmployeeWage empWage = new EmployeeWage();
+            //empWage.CalEmployee();
+
+            //EmpSwitchCase empSwitchCase = new EmpSwitchCase();
+            //empSwitchCase.CalEmployeeSwitch();
+
+            //EmpWagePerMonth empWagePerMonth = new EmpWagePerMonth();
+            //empWagePerMonth.CalEmployeeWagePerMonth();
+
+            //EmpMonthlyWageCalc empMonthlyWageCalc = new EmpMonthlyWageCalc();
+            //empMonthlyWageCalc.EmpWageCalcWithHoursOrDays();
+
+        }
+    }
+}
+
